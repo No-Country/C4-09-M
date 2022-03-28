3995830ede6155ba54d1bb37a4baab6c9a27f6be
Celso Zabala <celsobrc@gmail.com>
2022-03-28 09:29:05 -0300

    Initial Commit Backend

---
+++ src/main/java/com/ecommerce/changuito/ChanguitoApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.changuito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanguitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanguitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/changuito/ChanguitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.changuito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanguitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
