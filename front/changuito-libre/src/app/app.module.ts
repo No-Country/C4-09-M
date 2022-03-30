eedf7b30d7b65e00226389d4e43cd92c8e42c820
AlanFarias97 <alanfarias_97@hotmail.com>
2022-03-30 10:34:06 -0300

    Agrego Component Navbar

--- front/changuito-libre/src/app/app.module.ts
+++ front/changuito-libre/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductsCarouselComponent } from './components/products-carousel/products-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductsCarouselComponent
   ],
   imports: [
     BrowserModule,
