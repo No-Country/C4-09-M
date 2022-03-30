ca2c6ddefb5025a73e1d4e59e64de4b9777c4fa5
AlanFarias97 <alanfarias_97@hotmail.com>
2022-03-30 11:48:32 -0300

    fix problems and merge, agrego font awesome

--- front/changuito-libre/src/app/app.module.ts
+++ front/changuito-libre/src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { NgModule } from '@angular/core';
+import { LoguinComponent } from './components/loguin/loguin.component';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoguinComponent } from './components/loguin.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsCarouselComponent } from './components/products-carousel/products-carousel.component';
 
@@ -11,12 +10,8 @@ import { ProductsCarouselComponent } from './components/products-carousel/produc
   declarations: [
     AppComponent,
     NavbarComponent,
-<<<<<<< HEAD
-    LoguinComponent
-
-=======
+    LoguinComponent,
     ProductsCarouselComponent
->>>>>>> eedf7b30d7b65e00226389d4e43cd92c8e42c820
   ],
   imports: [
     BrowserModule,
