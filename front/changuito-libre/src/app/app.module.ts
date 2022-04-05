f08d6cbe632a241c387451a92b917b59ee039ec0
AlanFarias97 <alanfarias_97@hotmail.com>
2022-04-05 08:50:13 -0300

    Instalo bootstrap

--- front/changuito-libre/src/app/app.module.ts
+++ front/changuito-libre/src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { AdminReportesComponent } from './components/admin-dashboard/admin-reportes/admin-reportes.component';
+import { AdminSideBarComponent } from './components/admin-dashboard/admin-side-bar/admin-side-bar.component';
+import { AdminPerfilComponent } from './components/admin-dashboard/admin-perfil/admin-perfil.component';
+import { AdminNavComponent } from './components/admin-dashboard/admin-nav/admin-nav.component';
 import { LoguinComponent } from './components/loguin/loguin.component';
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -23,18 +27,8 @@ import { MerchMetodosPagoCrearComponent } from './components/merchandiser-dashbo
 import { MerchPerfilModificarComponent } from './components/merchandiser-dashboard/merch-perfil-modificar/merch-perfil-modificar.component';
 import { TeamComponent } from './components/team/team.component';
 import { HeroComponent } from './components/hero/hero.component';
-<<<<<<< HEAD
-import { AdminNavComponent } from './admin-dashboard/admin-nav/admin-nav.component';
-import { AdminVentasComponent } from './components/admin-dashboard/admin-ventas/admin-ventas.component';
-import { NavComponent } from './components/admin-dashboard/nav/nav.component';
-import { AdminPerfilComponent } from './components/admin-dashboard/admin-perfil/admin-perfil.component';
-import { AdminMerchComponent } from './components/admin-dashboard/admin-merch/admin-merch.component';
-import { AdminSideBarComponent } from './components/admin-dashboard/admin-side-bar/admin-side-bar.component';
-import { EditarPerfilComponent } from './components/admin-dashboard/editar-perfil/editar-perfil.component';
-import { AdminReportesComponent } from './components/admin-dashboard/admin-reportes/admin-reportes.component';
-=======
 import { CartComponent } from './components/cart/cart.component';
->>>>>>> c047dc962084fe7ff5f54899d6f529bf391ff2c9
+import { AdminMerchComponent } from './components/admin-dashboard/admin-merch/admin-merch.component';
 
 @NgModule({
   declarations: [
@@ -60,24 +54,18 @@ import { CartComponent } from './components/cart/cart.component';
     MerchPerfilModificarComponent,
     TeamComponent,
     HeroComponent,
-<<<<<<< HEAD
-    AdminNavComponent,
-    AdminVentasComponent,
-    NavComponent,
-    AdminPerfilComponent,
+    CartComponent,
     AdminMerchComponent,
+    AdminNavComponent,
+    AdminPerfilComponent,
     AdminSideBarComponent,
-    EditarPerfilComponent,
     AdminReportesComponent
-=======
-    CartComponent
->>>>>>> c047dc962084fe7ff5f54899d6f529bf391ff2c9
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [], 
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
