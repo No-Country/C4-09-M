05b49b4c2bd2712eb66e1e2de74045c2c52faca4
DaluPalma <plalu060@gmail.com>
2022-04-07 10:11:40 -0300

    Agrego control de usuarios registrados

--- front/changuito-libre/src/app/app.module.ts
+++ front/changuito-libre/src/app/app.module.ts
@@ -1,40 +1,12 @@
-import { AdminReportesComponent } from './components/admin-dashboard/admin-reportes/admin-reportes.component';
-import { AdminSideBarComponent } from './components/admin-dashboard/admin-side-bar/admin-side-bar.component';
-import { AdminPerfilComponent } from './components/admin-dashboard/admin-perfil/admin-perfil.component';
-import { AdminNavComponent } from './components/admin-dashboard/admin-nav/admin-nav.component';
-import { LoguinComponent } from './components/loguin/loguin.component';
-import { NgModule, Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ProductsCarouselComponent } from './components/products-carousel/products-carousel.component';
-import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
-import { MerchandiserDashboardComponent } from './components/merchandiser-dashboard/merchandiser-dashboard.component';
-import { RegistrationUsersComponent } from './components/registration-users/registration-users.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
-import { MerchNavComponent } from './components/merchandiser-dashboard/merch-nav/merch-nav.component';
-import { MerchSideBarComponent } from './components/merchandiser-dashboard/merch-side-bar/merch-side-bar.component';
-import { MerchProductsComponent } from './components/merchandiser-dashboard/merch-products/merch-products.component';
-import { MerchVentasComponent } from './components/merchandiser-dashboard/merch-ventas/merch-ventas.component';
-import { MerchReportesComponent } from './components/merchandiser-dashboard/merch-reportes/merch-reportes.component';
-import { MerchMetodosPagoComponent } from './components/merchandiser-dashboard/merch-metodos-pago/merch-metodos-pago.component';
-import { MerchPerfilComponent } from './components/merchandiser-dashboard/merch-perfil/merch-perfil.component';
-import { MerchProductoCrearComponent } from './components/merchandiser-dashboard/merch-producto-crear/merch-producto-crear.component';
-import { MerchProductoModificarComponent } from './components/merchandiser-dashboard/merch-producto-modificar/merch-producto-modificar.component';
-import { MerchMetodosPagoCrearComponent } from './components/merchandiser-dashboard/merch-metodos-pago-crear/merch-metodos-pago-crear.component';
-import { MerchPerfilModificarComponent } from './components/merchandiser-dashboard/merch-perfil-modificar/merch-perfil-modificar.component';
-import { TeamComponent } from './components/team/team.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { CartComponent } from './components/cart/cart.component';
 import { AdminMerchComponent } from './components/admin-dashboard/admin-merch/admin-merch.component';
-<<<<<<< HEAD
+import { ProductsComponent } from './components/products/products.component';
 import { AdminUsuariosRegistradosComponent } from './components/admin-dashboard/admin-usuarios-registrados/admin-usuarios-registrados.component';
 import { AdminPaymentsComponent } from './components/admin-dashboard/admin-payments/admin-payments.component';
-=======
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { ProductosViewComponent } from './components/productos-view/productos-view.component';
 import { ProductsComponent } from './components/products/products.component';
->>>>>>> 40446c55a9bc15d0326efef646f4b50cc5f1252a
 
 @NgModule({
   declarations: [
@@ -66,16 +38,13 @@ import { ProductsComponent } from './components/products/products.component';
     AdminPerfilComponent,
     AdminSideBarComponent,
     AdminReportesComponent,
-<<<<<<< HEAD
-    AdminUsuariosRegistradosComponent,
-    AdminPaymentsComponent
-=======
-    ProductsComponent
->>>>>>> 40446c55a9bc15d0326efef646f4b50cc5f1252a
+    ProductsComponent,
+    ProductosViewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IvyCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
